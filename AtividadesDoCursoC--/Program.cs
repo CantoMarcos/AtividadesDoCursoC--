cf61d51c0e83cc1cf137b39f9b457ed00c800088
CantoMarcos <marcoscanto.mp@gmail.com>
2024-04-10 12:04:48 -0400

    teste

---
+++ AtividadesDoCursoC--/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Curso
+{
+    class Curso
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
